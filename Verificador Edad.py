7f17a91e48acb536d88bc66e8a16b7d5ac4d7099
diegobarr <d1390xd.v@gmail.com>
2025-07-16 20:35:04 -0600

    Agregado de la segunda condicional y de una nueva variable name

--- Verificador Edad.py
+++ Verificador Edad.py
@@ -2,8 +2,11 @@ print("Bienvenido al verificador de  rangos de Edad")
 cant=int(input("Ingrese la cantidad de usuarios: "))
 
 for i in range(cant):
-
+    name=input("Ingrese su nombre: ")
     age=int(input("Ingrese su edad: "))
 
     if age<18:
         print("Usted es considerado un menor de edad")
+
+    elif age<=18 and age<=28:
+        print("Usted es considerado un joven")
