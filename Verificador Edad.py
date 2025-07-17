4c5261bbd2b62f60cc8b5e8ba18f5907fd048982
Melqui Garcia <monterrosomelqui39@gmail.com>
2025-07-16 21:50:34 -0600

    elif, agregado edades de 29 a 100

--- Verificador Edad.py
+++ Verificador Edad.py
@@ -1,6 +1,6 @@
-    elif 29 <= age <= 45:
+    elif age <=29 and age<= 45:
        age_range= "señor"
-    elif 46 <= age <= 100:
+    elif age<=46 age<= 100:
       age_range = "anciano"
     else:
       age_range = "Esta edad esta fuera del rango aceptado, lo sentimos"
