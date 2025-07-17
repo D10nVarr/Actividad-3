b0bfefd9ef1eb9919e7f6714c8c4c9e8ba71cd84
Melqui Garcia <monterrosomelqui39@gmail.com>
2025-07-16 21:20:15 -0600

    elif y else, agregado de comprobante de edasde de 29 a 100 con impresion de nombre edad y la etapa segun la edad

--- Verificador Edad.py
+++
@@ -1,7 +0,0 @@
-elif 29 <= edad <= 45:
-        etapa = "señor"
-    elif 46 <= edad <= 100:
-        etapa = "anciano"
-    else:
-        etapa = "Esta edad esta fuera del rango aceptado, lo sentimos"
-    print(f"{nombre}, edad {edad}: {etapa}.")
