fc9bc2d58d8f0ce9136917a383d5058cbfc037da
Melqui Garcia <monterrosomelqui39@gmail.com>
2025-07-16 20:12:30 -0600

    elif, comprobantes de las edades de 29 para 100

--- Verificador Edad.py
+++ Verificador Edad.py
@@ -1,1 +1,8 @@
-print("Bienvenido al verificador de  rangos de Edad")
+elif 29 <= edad <= 45:
+        etapa = "señor"
+    elif 46 <= edad <= 100:
+        etapa = "anciano"
+    else:
+        etapa = "Esta edad esta fuera del rango aceptado, lo sentimos"
+
+    print(f"{nombre}, edad {edad}: {etapa}.")
