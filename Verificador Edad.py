cf71aba7a6eba5387970216c37259ca06e9be37f
diegobarr <d1390xd.v@gmail.com>
2025-07-16 20:31:48 -0600

    Modificación la la condicional para funcionar dentro de un ciclo

--- Verificador Edad.py
+++ Verificador Edad.py
@@ -1,6 +1,9 @@
 print("Bienvenido al verificador de  rangos de Edad")
+cant=int(input("Ingrese la cantidad de usuarios: "))
 
-age=int(input("Ingrese su edad: "))
+for i in range(cant):
 
-if age<18:
-    print("Usted es considerado un menor de edad")
+    age=int(input("Ingrese su edad: "))
+
+    if age<18:
+        print("Usted es considerado un menor de edad")
